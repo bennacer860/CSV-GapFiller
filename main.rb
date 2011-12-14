15996cb370376d6439134f9a84da0fe86df96255
bennacer <bennacer860@gmail.com>
2011-12-14 12:12:49 -0500

    commit the script and the example

---
+++ main.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require "faster_csv"
+
+table=FasterCSV.read('test.csv', :headers => :first_row)
+
+
+puts "print new table"
+table.each{|row|
+  puts row
+}
+puts '*' * 20
+
+
+
+#we want to perform the modification on this cell index
+cell_index=0
+row=1
+column=1
+row_nb=table.size
+puts table[row][column].inspect
+
+#for each row of the table
+0.upto(row_nb-1) do |r|
+	#puts table[r].inspect
+	#if it is not the first cell and the  is empty 
+	if r >0 && table[r][cell_index].strip.empty?
+		table[r][cell_index]=table[r-1][cell_index]
+
+		puts " row #{r+1} needs to be edited with #{table[r-1][cell_index]}"
+	end
+		
+end
+
+
+puts '*' * 20
+
+puts "print new table"
+table.each{|row|
+  puts row
+}

e95d187c98ac94ddc070a00cc6410dcd9ebffa9a
bennacer <bennacer860@gmail.com>
2012-01-23 10:55:28 -0500

    comment debug lines

--- main.rb
+++ main.rb
@@ -4,20 +4,20 @@ require "faster_csv"
 table=FasterCSV.read('test.csv', :headers => :first_row)
 
 
-puts "print new table"
-table.each{|row|
-  puts row
-}
-puts '*' * 20
+#puts "print new table"
+#table.each{|row|
+#  puts row
+#}
+#puts '*' * 20
 
 
 
 #we want to perform the modification on this cell index
-cell_index=0
+cell_index=1
 row=1
 column=1
 row_nb=table.size
-puts table[row][column].inspect
+#puts table[row][column].inspect
 
 #for each row of the table
 0.upto(row_nb-1) do |r|
@@ -26,15 +26,15 @@ puts table[row][column].inspect
 	if r >0 && table[r][cell_index].strip.empty?
 		table[r][cell_index]=table[r-1][cell_index]
 
-		puts " row #{r+1} needs to be edited with #{table[r-1][cell_index]}"
+#		puts " row #{r+1} needs to be edited with #{table[r-1][cell_index]}"
 	end
 		
 end
 
 
-puts '*' * 20
+#puts '*' * 20
 
-puts "print new table"
+#puts "print new table"
 table.each{|row|
   puts row
 }
